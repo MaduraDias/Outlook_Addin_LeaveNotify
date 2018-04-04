6b23a8c95ac2c8638e1887c3df8ad9987ecffb4b
MaduraDias <diasmadura@yahoo.co.uk>
2018-04-05 08:41:27 +1000

    MockAPI developent

---
+++ MockApi/MockApi/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MockApi.Models;
+
+namespace MockApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[Controller]/")]
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        [Route("{id}/LeaveNotifyEmailList")]
+        public LeaveNotifyData LeaveNotifyEmailList([FromRoute]string id)
+        {
+            return new LeaveNotifyData()
+            {
+                 To = "nick.hughes@readify.net",
+                 CC = new string[] {"narges.ghorbani@readify.net"
+                                 ,"Parma.Juss@readify.net"
+                                 ,"richard.banks@readify.net"}
+            };
+
+        }
+    }
+
+}
---
+++ MockApi/MockApi/Models/LeaveNotifyData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockApi.Models
+{
+    public class LeaveNotifyData
+    {
+        public string To { get; set; }
+
+        public IEnumerable<string> CC { get; set; }
+    }
+}
