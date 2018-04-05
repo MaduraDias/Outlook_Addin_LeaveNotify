93a1436e484b2a09ddfcae4f53896acc448ffcb3
MaduraDias <diasmadura@yahoo.co.uk>
2018-04-05 11:47:31 +1000

    Adding styles to the addin

--- outlook_slack_addin_app/src/App.js
+++ outlook_slack_addin_app/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//Importing Layout classes  https://developer.microsoft.com/en-us/fabric#/styles/layout
+import 'office-ui-fabric-core/dist/css/fabric.min.css'
+
 import { DatePicker, DayOfWeek, IDatePickerStrings } from 'office-ui-fabric-react/lib/DatePicker';
 import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
@@ -8,7 +11,8 @@ import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { Label } from 'office-ui-fabric-react/lib/Label';
 import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
 import  axios from 'axios';
-
+import {styles,ColorClassNames,} from '@uifabric/styling';
+import { initializeIcons } from '@uifabric/icons';
 
 class App extends Component {
 
@@ -21,51 +25,47 @@ class App extends Component {
     this.setSubject("Leave Request");
     
     this.getEmailList();
+
+    //Without this line Datepicker calendar icon and dropdown caret icon does not display
+    initializeIcons();
   }
 
+ emailDetails = {
+   subject:'',
+   reason:'',
+   startDate:new Date(),
+   endDate:new Date(),
+   leaveType:'',
+ };
 
   setSubject = (subject)=>{
-    //window.Office.context.mailbox.item.subject = 'set the value';
-    this.setState({ subject: subject})
+    this.emailDetails.subject = subject;
     this.officeMailBoxItem.subject.setAsync(subject);
-    
   };
-
-   setToday = ()=>{
-    //window.Office.context.mailbox.item.subject = 'set the value';
-     let today = new Date();
-     this.setState({ startDateTime: today })
-     this.officeMailBoxItem.start.setAsync(today );
-     this.officeMailBoxItem.end.setAsync(today);
-  };
-
-
-   setStartdDate = (value)=>{
-    //window.Office.context.mailbox.item.subject = 'set the value';
-    // this.setState({ value: date });
+   
+   setStartDate = (value)=>{
      let date = new Date(value);
-     this.setState({ startDateTime: date })
+    this.emailDetails.startDate;
   };
 
   setEndDate = (value)=>{
     let date = new Date(value);
-    this.setState({ endDateTime: date })
-   // this.officeMailBoxItem.end.setAsync(date);
-  };
+    this.emailDetails.endDate = date;
+    };
 
   setLeaveType = (option) => {
-    this.setState({ leaveType: option })
+    this.emailDetails.leaveType = option;
   };
 
   setReason = (value) => {
-    this.setState({ reason : value })
+    this.emailDetails.reason = value;
   };
 
-  setMessage = ()=>{
+  createMessage = ()=>{
     this.officeMailBoxItem.body.setAsync(
-      '<div>From :' + this.state.startDateTime + ' From Date:' + this.state.endDateTime + '</div>' +
-      '<div>Leave Type :' + this.state.leaveType.text + '</div>' +
-      '<div>Reason :' + this.state.reason + '</div>',
+      '<div>From :' + this.emailDetails.startDate.toLocaleDateString() + ' To:' + this.emailDetails.endDate.toLocaleDateString() + '</div>' +
+      '<div>Leave Type :' + this.emailDetails.leaveType.text + '</div>' +
+      '<div>Reason :' + this.emailDetails.reason + '</div>',
      { coercionType: this.office.CoercionType.Html })
   };
   
@@ -80,31 +80,70 @@ class App extends Component {
       .then(function (response) {
        setEmail(response.data);
       }).catch(function (error) {
-        setEmail(error);
+         //log
       });
 
   };
 
   render() {
     return (
-      <Fabric>
-            <TextField label='Reason'  onChanged={this.setReason} /> 
-            <DatePicker label='Start Date' onSelectDate={this.setStartdDate} />
-            <DatePicker label='End Date' onSelectDate={this.setEndDate} />
-            <Dropdown label='Leave Type' onChanged={this.setLeaveType} options={[
+      <div id="addInContainerDiv"  class= 'ms-grid'>
+       
+          <div class = "ms-Grid-row" >
+            <DatePicker className = "ms-Grid-col ms-sm4 ms-lg4"
+            name = "Start Date"
+            placeholder = 'Select the From Date'
+            onSelectDate = {
+              this.setStartDat
+            }
+            />
+           </div>
+          
+          <div class = "ms-Grid-row" >
+            <DatePicker className = "ms-Grid-col ms-sm4 ms-lg4"
+            name = "End Date"
+            placeholder = 'Select the To Date'
+            onSelectDate = {
+             this.setEndDate
+            }
+            />
+          </div>
+
+           <div class = "ms-Grid-row" >
+            <Dropdown className = "ms-Grid-col ms-sm4 ms-lg4"
+            placeHolder = 'Select Leave Type'
+            onChanged = {
+              this.setLeaveType
+            }
+            options = {
+                [
               {key:'annual',text:'Annual Leave'},
               {key:'personal',text:'Sick/Carer Leave'}
             ]} > </Dropdown>
-            <DefaultButton primary={ true } onClick={this.setToday} >
-              Set Today
-            </DefaultButton>
+            </div>
 
-           <DefaultButton primary={true} onClick={this.setMessage} >
-              Ok
-            </DefaultButton>
+        <div class="ms-Grid-row" >
+          <TextField className="ms-Grid-col ms-sm4 ms-lg4"
+            Label='Reason'
+            onChanged={
+              this.setReason
+            }
+            multiline
+            rows = {5}
+            autoAdjustHeight
+          />
+        </div>
 
 
-      </Fabric>
+           <div class = "ms-Grid-row" >
+           <div id="buttonContainerDiv" className="ms-Grid-col ms-sm4 ms-lg4">
+              <DefaultButton id='OKButton' primary={true} onClick={this.createMessage} >
+                OK
+              </DefaultButton>
+            </div>
+           </div>
+
+      </div>
     );
   }
 }
